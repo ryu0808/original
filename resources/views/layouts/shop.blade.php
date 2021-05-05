ba1628c51b717fd217e18d6209d09630d12a61b6
ryuhei <jkkrs1221185@gmail.com>
2021-05-05 13:19:15 +0000

    ヘッダー文字色変更

--- resources/views/layouts/shop.blade.php
+++ resources/views/layouts/shop.blade.php
@@ -29,9 +29,7 @@
             {{-- 画面上部に表示するナビゲーションバー --}}
             <nav class="navbar navbar-expand-md navbar-dark navbar-laravel">
                 <div class="container">
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
-                    </a>
+                   
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
