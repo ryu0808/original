6f2d74f7c92c554ab3c2eb77930b89e940046f55
ryuhei <jkkrs1221185@gmail.com>
2021-05-09 04:54:49 +0000

    画像表示修正後

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/','HomeController@index');
 
 Route::group(['prefix' =>'shop'],function() {
     Route::get('animal/create', 'Shop\AnimalController@add')->middleware('auth');
